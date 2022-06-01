989590fff65b8cb48d449da4bb11e615d6873b1f
Emilio0404 <emilio.alvear@hotmail.com>
2022-06-01 18:29:26 -0500

    Se agrego escritura en archivos HTML

--- resaltadorSintaxisParalelo.go
+++ resaltadorSintaxisParalelo.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"os"
 )
 
@@ -35,11 +34,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	directorioActual, err := os.Getwd()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(2)
-	}
+	directorioActual := obtenerDirectorioActual()
 
 	for i := 0; i < noArgumentos; i++ {
 		if !revisarFormatoArchivo(nombresArchivos[i]) {
@@ -47,7 +42,7 @@ func main() {
 			os.Exit(3)
 		}
 
-		if !revisarArchivoExiste(nombresArchivos[i], directorioActual) {
+		if !archivoExiste(nombresArchivos[i], directorioActual) {
 			fmt.Println("ERROR: DEBE PROVEER ARCHIVOS EXISTENTES")
 			os.Exit(4)
 		}
@@ -58,42 +53,73 @@ func main() {
 	}
 }
 
-func revisarFormatoArchivo(archivo string) bool {
-	extensionArchivo := archivo[len(archivo)-4:]
-	return extensionArchivo == ".txt"
+func obtenerDirectorioActual() string {
+	directorioActual, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	return directorioActual
 }
 
-func revisarArchivoExiste(archivo string, directorioActual string) bool {
+func revisarFormatoArchivo(archivo string) bool {
+	if len(archivo) < 4 {
+		return false
+	}
+	return archivo[len(archivo)-4:] == ".txt"
+}
+
+func archivoExiste(archivo string, directorioActual string) bool {
 	_, err := os.Stat(directorioActual + "\\" + archivo)
 	if os.IsNotExist(err) {
 		return false
 	}
-
 	return true
 }
 
 func resaltadorSintaxis(archivo string) {
+	// Crear archivo HTML
+	nombreArchivoHTML := crearArchivoHTML(archivo)
+	archivoHTML, err := os.Open(nombreArchivoHTML)
+	check_error(err)
+	defer archivoHTML.Close()
 
-	file, err := os.Open(archivo)
-	if err != nil {
-		fmt.Println("Error opening file!!!")
+	codigoResaltado := "<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<meta charset=\"utf-8\"/>\n\t\t<link rel=\"stylesheet\" href=\"styles.css\">\n\t</head>\n\t<body>\n"
+	codigoResaltado += resaltar()
+	codigoResaltado += "\n\t</body>\n</html>"
+
+	escribirCodigoResaltado(nombreArchivoHTML, codigoResaltado)
+}
+
+// Crea un archivo HTML con el nombre de archivo que recibe. Si el
+// archivo ya existe, lo limpia
+func crearArchivoHTML(archivo string) string {
+	nombreArchivoHTML := archivo[:len(archivo)-4] + ".html"
+
+	if archivoExiste(nombreArchivoHTML, obtenerDirectorioActual()) {
+		err := os.Truncate(nombreArchivoHTML, 0)
+		check_error(err)
+	} else {
+		archivoHTML, err := os.Create(nombreArchivoHTML)
+		check_error(err)
+		defer archivoHTML.Close()
 	}
-	defer file.Close()
 
-	const maxSz = 1
+	return nombreArchivoHTML
+}
 
-	// create buffer
-	b := make([]byte, maxSz)
+func resaltar() string {
+	return "hello world"
+}
 
-	for {
-		// read content to buffer
-		readTotal, err := file.Read(b)
-		if err != nil {
-			if err != io.EOF {
-				fmt.Println(err)
-			}
-			break
-		}
-		fmt.Println(string(b[:readTotal])) // print content from buffer
+// Escribe el codigo resaltado en un archivo
+func escribirCodigoResaltado(nombreArchivo string, codigoResaltado string) {
+	err := os.WriteFile(nombreArchivo, []byte(codigoResaltado), 0644)
+	check_error(err)
+}
+
+func check_error(e error) {
+	if e != nil {
+		fmt.Println(e)
 	}
 }
